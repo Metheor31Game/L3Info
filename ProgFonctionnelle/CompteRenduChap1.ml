3c33c7a471e04afc82f888c7f1f305bb1573086d
matheo <matheo>
2024-09-13 16:16:09 +0200

    Chapitre1

--- ProgFonctionnelle/CompteRenduChap1.ml
+++ ProgFonctionnelle/CompteRenduChap1.ml
@@ -116,7 +116,7 @@ true || false && false;;
  (* 2.2 les conditions *)
 
 
- if 1<3 then "hello world";;
+ if 1<3 then "hello world" else "nope";;
  (* il faut absolument un else *)
 
  if "A" < "B" then 1 else 3;;
@@ -125,3 +125,63 @@ true || false && false;;
  if 2.0 < 3.5 then 2 else 4;;
 
  (* vrai donc résultat est 2*);;
+ let a = true;;
+ let b = false;;
+
+ if a then true else false;;
+
+ (* A *)
+ if a then true else b;;
+
+ (* A || B*)
+
+ if a then false else b;;
+
+ (* not(A) && B *)
+
+ if a then b else true;;
+
+ (* B || not(A) *)
+
+ if a then b else true;;
+ 
+ (* A && B *)
+
+
+ (* 3.3 Environnement *)
+
+ let annee = 2020;; (* 2020 *)
+ annee;; (* 2020 *)
+ annee + 2 ;; (* 2022 *)
+ let futur = 2021;; (* 2021 *)
+ futur = annee + 1;; (* true *)
+ (*annee_future;;*) (* erreur *)
+ let annee_future = annee + 1;; (* 2021 *)
+ let annee = 2000;; (* 2000 *)
+ annee_future;; (* 2021 *)
+
+ (* Environnement 
+		[(annee,2020)]
+		[(futur,2021),(annee,2020)]
+		[(futur,2021),(futur,2021),(annee,2020)]
+		[(annee_future,2021),(futur,2021),(annee,2020)]
+		[(annee,2000),(annee_future,2021),(futur,2021),(annee,2020)]
+ *)
+
+ let va = 8;;
+ let double = va * 2 and triple = va*3;;
+ (*let moitie = va/2 and quart = moitie/2;;
+ moitie;;*)
+
+ (* Environnement 
+	[(va,8)]
+	[(triple,va*3),(double,va*2),(va,8)]
+ 
+ *)
+
+ 
+
+
+
+
+
