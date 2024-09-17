901cccd5a21b2f91cabf71dc1a94b772e35cd386
matheo <matheo>
2024-09-17 10:03:45 +0200

    tp3

---
+++ TW3/JohanGirod/TP2/memory/src/index.ts
@@ -0,0 +1,13 @@
+console.log("Hello world");
+
+type emoji = '🍎' | '🍌' | '🍒' | '🍓' | '🍇' | '🍉' | '🍊' | '🍍';
+
+type carte = {
+    symbole: emoji;
+    etat: boolean; 
+}
+
+
+function creerJeu(): Array<carte> {
+    
+}
