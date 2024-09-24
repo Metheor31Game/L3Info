bdd7f1850246e759c4ecf07ad3870e3de5a7baf5
matheo <matheo>
2024-09-24 17:45:30 +0200

    debut chap 4

--- ProgFonctionnelle/compteRenduChap4.ml
+++ ProgFonctionnelle/compteRenduChap4.ml
@@ -14,3 +14,49 @@ let et = function
    (false,_) -> false
   |(_,false) -> false
   | _ -> true;;
+
+let ou = function
+   (false,false) -> false
+  |_ -> true;;
+
+let xor = function
+   (true,true) -> false
+  |(false,false) -> false
+  |_ -> true;;
+
+
+(*Exercice 2*)
+
+let entier = function 
+  0 -> "zero"
+  |1 -> "un"
+  |x -> if x mod 2 = 0 then "pair" else "impair";;
+  
+(*Exercice 3*)
+
+let point = function
+  (0.,0.) -> "Origine" |
+  (0.,_) -> "Axe des ordonnées" |
+  (_,0.) -> "Axe des abscisses" |
+  (x,y) -> if x > 0 then "point du demi plan x>0" else "point du demi plan x<0";;
+
+(*Exercice 4*)
+
+let operation = function
+  (x,y,'+') -> x + y |
+  (x,y,'-') -> x - y |
+  (x,y,'*') -> x * y |
+  (x,0,'/') -> failwith "ERREUR tu vas mourir" |
+  (x,y,'/') -> x / y |
+  (x,y,_) -> failwith "ERREUR tu pourrais mourir";;
+
+
+(*Exercice 5*)
+
+let prixttc = function (prix, tva) -> prix +. prix*.tva;;
+
+let prix = function 
+  ("pain") -> prixttc(1.05) 
+
+  CONTINUER !!!!!!!!!!!!!!!!!!!!
+
