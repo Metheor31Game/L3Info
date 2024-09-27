746b89be9284bef7c4864dc2137c1d70689134dd
matheo <matheo>
2024-09-27 15:23:59 +0200

    fin TP4

--- ProgFonctionnelle/compteRenduChap4.ml
+++ ProgFonctionnelle/compteRenduChap4.ml
@@ -55,8 +55,52 @@ let operation = function
 
 let prixttc = function (prix, tva) -> prix +. prix*.tva;;
 
-let prix = function 
-  ("pain") -> prixttc(1.05) 
+let prix = function
+  ("pain") -> prixttc(1.05,5.5) |
+  ("conserve") -> prixttc(3.5,7.0) |
+  ("disque") -> prixttc(12.30,18.6) |
+  ("bijou") -> prixttc(356.,33.) |
+  (_) -> failwith "je connais pas";;
 
-  CONTINUER !!!!!!!!!!!!!!!!!!!!
+let troncature = function x -> int_of_float(x);;
 
+let decimale = function x -> abs_float(x) -. float_of_int(troncature(abs_float(x)));;
+
+
+let plus_proche_entier = function x -> 
+if decimale(x) >= 0.5 then troncature(x) + 1
+else troncature(x);;
+
+let sommeAPayer = function (element, nombre) -> plus_proche_entier(prix(element) *. float_of_int(nombre));;
+
+(*Exercice 6*)
+
+let formule = function (j,m,p,s) -> j + (48*m - 1)/5 + p/4 + p + s/4 -2*s;;
+
+let decoupe = function (annee) -> annee/100 , annee mod 100;;
+
+let deuxMoisAvant = function 
+  (2,annee) -> 12, annee - 1 |
+  (1,annee) -> 11, annee -1 |
+  (mois,annee) -> mois - 2, annee;;
+
+  let jour = function 
+   0 -> "Dimanche"
+  | 1 -> "Lundi"
+  | 2 -> "Mardi"
+  | 3 -> "Mercredi"
+  | 4 -> "Jeudi"
+  | 5 -> "Vendredi"
+  | 6 -> "Samedi"
+  | _ -> failwith "Invalid day";;
+
+let modulo7 = function (nombre) -> if nombre >= 0 then  nombre mod 7 else (nombre mod 7) + 7;;
+
+let quelJour = function (j,mois,annee) -> 
+  let m,a = deuxMoisAvant(mois,annee) in
+  let s,p = decoupe(a) in
+  let k = formule(j,m,p,s) in
+  let modulo7dek = modulo7(k) in
+  jour(modulo7dek);;
+
+quelJour(14,7,1789);;
