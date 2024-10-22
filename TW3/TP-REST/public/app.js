053f153698dabe2e7a598fe4a4db4ea545911d2e
matheo <matheo>
2024-10-22 09:56:20 +0200

    TP-REST

--- TW3/TP-REST/public/app.js
+++ TW3/TP-REST/public/app.js
@@ -1,34 +1,52 @@
-import { createMovieCard } from './movie-card.js'
+import { createMovieCard } from "./movie-card.js";
 async function main() {
-    const input = document.querySelector('[data-search-input]');
-    const movies = await getMovies("");
+  const input = document.querySelector("[data-search-input]");
+  const movies = await getMovies("");
+  renderMovies(movies);
+
+  input.addEventListener("input", async (event) => {
+    const valeur = event.target.value;
+    const movies = await getMovies(valeur);
     renderMovies(movies);
-    
-
-    input.addEventListener('input', async (event) => {
-        const valeur = event.target.value;
-        const movies = await getMovies(valeur);
-        renderMovies(movies);
-    });
-
-
+  });
 }
 
-
 async function getMovies(query) {
-    const response = await fetch(`/movies?query=${query}`);
-    const movies = await response.json();
-    return movies
+  const response = await fetch(`/movies?query=${query}`);
+  const movies = await response.json();
+
+  for (const movie of movies) {
+    const libraryMovie = await getLibraryMovie(movie.id);
+    console.log(libraryMovie);
+    if (libraryMovie) {
+      movie.isInLibrary = true;
+      movie.rating = libraryMovie.rating;
+    } else {
+      movie.isInLibrary = false;
+    }
+  }
+  console.table(movies);
+
+  return movies;
 }
 
 async function renderMovies(movies) {
-    let container = document.querySelector('[data-movies]');
-    container.innerHTML = "";
-    movies.forEach(element => {
-        const carte = createMovieCard({movie:element});
-        container.appendChild(carte);
-    });
+  let container = document.querySelector("[data-movies]");
+  container.innerHTML = "";
+  movies.forEach((element) => {
+    const carte = createMovieCard({ movie: element });
+    container.appendChild(carte);
+  });
 }
 
+async function getLibraryMovie(id) {
+  const reponse = await fetch(`/library/${id}`);
+  if (reponse.status != 404) {
+    const movie = await reponse.json();
+    return movie;
+  } else {
+    return false;
+  }
+}
 
 main();
--- TW3/TP-REST/server/routes/library.ts
+++ TW3/TP-REST/server/routes/library.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import { getLibraryMovieById } from "../models/library";
 
 const router = express.Router();
 
 export default router;
+
+
+router.get("/library/:movieId", async (req,res) => {
+    const id = req.params.movieId;
+
+    try {
+        const movie = await getLibraryMovieById(id);
+        res.json(movie);
+    } catch {
+        res.status(404).send("Not Found !");
+    }
+
+});
