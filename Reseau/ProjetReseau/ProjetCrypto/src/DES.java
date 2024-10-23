092aeddcd7f4b469049706cb6e65c92efbec73f2
matheo <matheo>
2024-10-23 21:15:36 +0200

    decryptage qui fonctionne pas

--- Reseau/ProjetReseau/ProjetCrypto/src/DES.java
+++ Reseau/ProjetReseau/ProjetCrypto/src/DES.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class DES {
@@ -49,22 +50,28 @@ public class DES {
         message_code.add(Character.getNumericValue(bit));
       }
     }
+    // Compléter avec des zéros pour atteindre un multiple de 64 bits
+    while (message_code.size() % 64 != 0) {
+      message_code.add(0);
+    }
     return message_code;
   }
 
   public String bitsToString(ArrayList<Integer> message_code) {
     StringBuilder message_clair = new StringBuilder();
-    String parts = "";
-    for (int i = 0; i < message_code.size(); i++) {
-      parts = parts + message_code.get(i);
-      if (i % 8 == 7 || i == message_code.size() - 1) {
-        while (parts.length() < 8) {
-          parts = "0" + parts;
-        }
-        int charCode = Integer.parseInt(parts, 2);
-        message_clair.append((char) charCode);
-        parts = "";
+    int size = message_code.size();
+
+    // Vérifiez que la taille de message_code est un multiple de 8
+    if (size % 8 != 0) {
+      throw new IllegalArgumentException("La taille de message_code doit être un multiple de 8");
+    }
+
+    for (int i = 0; i < size; i += 8) {
+      int charCode = 0;
+      for (int j = 0; j < 8; j++) {
+        charCode = (charCode << 1) | message_code.get(i + j);
       }
+      message_clair.append((char) charCode);
     }
     return message_clair.toString();
   }
@@ -73,26 +80,29 @@ public class DES {
     DES.masterKey.clear();
     for (int i = 0; i < 64; i++) {
       DES.masterKey.add(new Random().nextInt(2));
+      // DES.masterKey.add(1);
     }
     return DES.masterKey;
   }
 
-  public ArrayList<Integer> permutation(int[] tab_permuation, ArrayList<Integer> bloc) {
-    ArrayList<Integer> bloc_perm = new ArrayList<>();
-    for (int i = 0; i < tab_permuation.length; i++) {
-      int nouvel_element = bloc.get(tab_permuation[i] - 1);
+  public ArrayList<Integer> permutation(int[] tab_permutation, ArrayList<Integer> bloc) {
+    ArrayList<Integer> bloc_perm = new ArrayList<>(tab_permutation.length);
+    for (int i = 0; i < tab_permutation.length; i++) {
+      int index = tab_permutation[i] - 1;
+      if (index < 0 || index >= bloc.size()) {
+        throw new IllegalArgumentException("Index de permutation invalide : " + index);
+      }
+      int nouvel_element = bloc.get(index);
       bloc_perm.add(nouvel_element);
     }
     return bloc_perm;
   }
 
-  public ArrayList<Integer> inv_permutation(int[] tab_permuation, ArrayList<Integer> bloc) {
-    ArrayList<Integer> bloc_perm = new ArrayList<>();
-    for (int i = 0; i < 64; i++) {
-      bloc_perm.add(0);
-    }
-    for (int i = 0; i < tab_permuation.length; i++) {
-      bloc_perm.set(i, bloc.get(tab_permuation[i] - 1));
+  public ArrayList<Integer> inv_permutation(int[] tab_permutation, ArrayList<Integer> bloc) {
+    ArrayList<Integer> bloc_perm = new ArrayList<>(Collections.nCopies(64, 0)); // Crée une liste remplie de 0
+    for (int i = 0; i < tab_permutation.length; i++) {
+      int index = tab_permutation[i] - 1;
+      bloc_perm.set(index, bloc.get(i)); // Applique la permutation correcte
     }
     return bloc_perm;
   }
@@ -124,11 +134,8 @@ public class DES {
   }
 
   public ArrayList<Integer> decallage_gauche1(ArrayList<Integer> minicle) {
-    ArrayList<Integer> nouvMiniCle = new ArrayList<>();
-    for (int i = 1; i < minicle.size(); i++) { // Commence à l'indice 1 pour ignorer le premier élément
-      nouvMiniCle.add(minicle.get(i));
-    }
-    nouvMiniCle.add(0); // Ajoute un 0 à la fin
+    ArrayList<Integer> nouvMiniCle = new ArrayList<>(minicle.subList(1, minicle.size())); // Décalage
+    nouvMiniCle.add(minicle.get(0)); // Ajoute le premier élément à la fin pour une rotation circulaire
     return nouvMiniCle;
   }
 
@@ -142,6 +149,9 @@ public class DES {
   }
 
   public ArrayList<Integer> xor(ArrayList<Integer> a, ArrayList<Integer> b) {
+    if (a.size() != b.size()) {
+      throw new IllegalArgumentException("Les deux tableaux doivent avoir la même taille pour effectuer un XOR");
+    }
     ArrayList<Integer> res = new ArrayList<>();
     for (int i = 0; i < a.size(); i++) {
       res.add(a.get(i) ^ b.get(i));
@@ -151,15 +161,19 @@ public class DES {
 
   public void genereCle(int n) {
     ArrayList<Integer> cle = DES.masterKey;
-    ArrayList<Integer> cle_perm = permutation(DES.PC1, cle);
+    ArrayList<Integer> cle_perm = permutation(DES.PC1, cle); // Permutation initiale PC1
     ArrayList<ArrayList<Integer>> blocs = decoupage(cle_perm, 28);
     ArrayList<ArrayList<Integer>> blocs2 = new ArrayList<>();
+
+    // Décalage circulaire des blocs
     for (ArrayList<Integer> bloc : blocs) {
-      blocs2.add(decallage_gauche(bloc, n));
+      blocs2.add(decallage_gauche(bloc, n)); // Applique le décalage avec le nombre de crans n
     }
+
+    // Recollage et permutation PC2 pour générer la clé ronde
     ArrayList<Integer> blocRecolles = recollage(blocs2);
     ArrayList<Integer> cle_i = permutation(PC2, blocRecolles);
-    DES.tab_cles.add(cle_i);
+    DES.tab_cles.add(cle_i); // Ajoute la clé générée à la liste des clés de rondes
   }
 
   public ArrayList<Integer> fonction_S(ArrayList<Integer> tableau) {
@@ -194,36 +208,89 @@ public class DES {
 
   }
 
-  public String cryptage(String message_clair) {
+  public String crypte(String message_clair) {
     this.genereMasterKey();
     ArrayList<Integer> message_code = this.stringToBits(message_clair);
+    System.out.println("Message en bits : " + message_code);
+
     ArrayList<ArrayList<Integer>> blocs = this.decoupage(message_code, 64);
     ArrayList<Integer> bloc_final = new ArrayList<>();
     for (ArrayList<Integer> bloc : blocs) {
       ArrayList<Integer> bloc_perm = this.permutation(DES.perm_initiale, bloc);
+      System.out.println("Bloc après permutation initiale : " + bloc_perm);
+
       ArrayList<ArrayList<Integer>> bloc_droit_gauche = this.decoupage(bloc_perm, 32);
       ArrayList<Integer> bloc_gauche = bloc_droit_gauche.get(0);
       ArrayList<Integer> bloc_droit = bloc_droit_gauche.get(1);
-      // for (int i = 0; i < DES.nb_ronde; i++) {
-      // DES.rondeActuelle = i;
-      ArrayList<Integer> bloc_gauche_temp = bloc_gauche;
-      bloc_gauche = bloc_droit;
-      bloc_droit = xor(bloc_gauche_temp, fonction_f(bloc_droit));
-      // }
-      bloc_final.removeAll(bloc_final);
+
+      // Boucle pour effectuer le nombre de rondes spécifié par nb_ronde
+      for (int i = 0; i < DES.nb_ronde; i++) {
+        this.genereCle(i);
+        DES.rondeActuelle = i;
+        ArrayList<Integer> bloc_gauche_temp = new ArrayList<>(bloc_gauche);
+        bloc_gauche = bloc_droit;
+        bloc_droit = xor(bloc_gauche_temp, fonction_f(bloc_droit));
+        System.out
+            .println("Après ronde " + (i + 1) + " : bloc_gauche = " + bloc_gauche + ", bloc_droit = " + bloc_droit);
+      }
+
       bloc_final.addAll(bloc_droit);
       bloc_final.addAll(bloc_gauche);
     }
     ArrayList<Integer> bloc_perm_inv = this.permutation(DES.perm_initiale_inv, bloc_final);
-    return this.bitsToString(bloc_perm_inv);
+    System.out.println("Bloc après permutation inverse : " + bloc_perm_inv);
 
+    String resultat = this.bitsToString(bloc_perm_inv);
+    System.out.println("Résultat final : " + resultat);
+    return resultat;
+  }
+
+  public String decrypte(String message_code) {
+    ArrayList<Integer> message_bits = this.stringToBits(message_code);
+    System.out.println("Message en bits : " + message_bits);
+
+    ArrayList<ArrayList<Integer>> blocs = this.decoupage(message_bits, 64);
+    ArrayList<Integer> bloc_final = new ArrayList<>();
+    for (ArrayList<Integer> bloc : blocs) {
+      ArrayList<Integer> bloc_perm = this.permutation(DES.perm_initiale_inv, bloc);
+      System.out.println("Bloc après permutation initiale : " + bloc_perm);
+
+      ArrayList<ArrayList<Integer>> bloc_droit_gauche = this.decoupage(bloc_perm, 32);
+      ArrayList<Integer> bloc_gauche = bloc_droit_gauche.get(0);
+      ArrayList<Integer> bloc_droit = bloc_droit_gauche.get(1);
+
+      // Boucle pour effectuer le nombre de rondes spécifié par nb_ronde en ordre
+      // inverse
+      for (int i = DES.nb_ronde - 1; i >= 0; i--) {
+        this.genereCle(i); // Utilise les sous-clés dans l'ordre inverse
+        DES.rondeActuelle = i;
+        ArrayList<Integer> bloc_droit_temp = new ArrayList<>(bloc_droit);
+        bloc_droit = bloc_gauche;
+        bloc_gauche = xor(bloc_droit_temp, fonction_f(bloc_droit));
+        System.out.println(
+            "Après ronde " + (DES.nb_ronde - i) + " : bloc_gauche = " + bloc_gauche + ", bloc_droit = " + bloc_droit);
+      }
+
+      bloc_final.addAll(bloc_droit);
+      bloc_final.addAll(bloc_gauche);
+    }
+    ArrayList<Integer> bloc_perm_inv = this.permutation(DES.perm_initiale, bloc_final);
+    System.out.println("Bloc après permutation inverse : " + bloc_perm_inv);
+
+    String resultat = this.bitsToString(bloc_perm_inv);
+    System.out.println("Résultat final : " + resultat);
+    return resultat;
   }
 
   public static void main(String[] args) {
     DES des = new DES();
     String message_clair = "Bonjour";
-    String message_code = des.cryptage(message_clair);
+    String message_code = des.crypte(message_clair);
     System.out.println(message_code);
+
+    String message_clair2 = des.decrypte(message_code);
+    System.out.println(message_clair2);
+    System.out.println(message_clair2.length());
 
   }
 
--- Reseau/ProjetReseau/ProjetCrypto/src/MasterKeyTest.java
+++ Reseau/ProjetReseau/ProjetCrypto/src/MasterKeyTest.java
@@ -44,31 +44,41 @@ public class MasterKeyTest {
 
     }
 
-    // @Test
-    // public void testDecoupage() {
-    // DES des = new DES();
-    // ArrayList<Integer> bloc_perm = new ArrayList<>();
-    // Random r = new Random();
-    // for (int i = 0; i < 1225; i++) {
-    // bloc_perm.add(r.nextInt(2));
-    // }
-    // ArrayList<ArrayList<Integer>> blocs = des.decoupage(bloc_perm, 64);
-    // assertEquals(20, blocs.size());
+    @Test
+    public void testDecoupage() {
+        DES des = new DES();
 
-    // ArrayList<Integer> nouveaubloc = des.recollage(blocs);
+        // Bloc original
+        ArrayList<Integer> bloc = new ArrayList<>(Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8,
+                9, 10, 11, 12, 13, 14, 15, 16,
+                17, 18, 19, 20));
+        int taille_sous_bloc = 8;
 
-    // assertEquals(bloc_perm, nouveaubloc);
+        // Découpage
+        ArrayList<ArrayList<Integer>> blocs = des.decoupage(bloc, taille_sous_bloc);
 
-    // }
+        // Vérification des sous-blocs
+        assertEquals(3, blocs.size());
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), blocs.get(0));
+        assertEquals(Arrays.asList(9, 10, 11, 12, 13, 14, 15, 16), blocs.get(1));
+        assertEquals(Arrays.asList(17, 18, 19, 20, 0, 0, 0, 0), blocs.get(2));
+
+        // Recollage
+        ArrayList<Integer> nouveau_bloc = des.recollage(blocs);
+
+        // Vérification du bloc recollé
+        assertEquals(bloc, nouveau_bloc);
+    }
 
     @Test
     public void testDecallage() {
         DES des = new DES();
         ArrayList<Integer> minicle = new ArrayList<>(Arrays.asList(1, 0, 0, 1));
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(0, 0, 1, 0));
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(0, 0, 1, 1));
         assertEquals(expected, des.decallage_gauche(minicle, 1));
         minicle = new ArrayList<>(Arrays.asList(0, 1, 1, 1));
-        expected = new ArrayList<>(Arrays.asList(1, 1, 0, 0));
+        expected = new ArrayList<>(Arrays.asList(1, 1, 0, 1));
         assertEquals(expected, des.decallage_gauche(minicle, 2));
 
     }
